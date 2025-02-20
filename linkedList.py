ea92a0f1be93e73d5f82cdc3533a8ce073e0e5c4
Gitter150 <chinmaypanghri@gmail.com>
2025-02-20 16:29:25 +0530

    Significantly improved linkedList.py

--- linkedList.py
+++ linkedList.py
@@ -2,6 +2,8 @@ class doublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.size = 0
+
     class Node:
         def __init__(self, x):
             self.data = x
@@ -16,8 +18,9 @@ class doublyLinkedList:
         else:
             self.tail = newNode
         self.head = newNode
+        self.size += 1
         
-    def push_back(self, x: int):
+    def push(self, x: int):
         newNode = self.Node(x)
         newNode.prev = self.tail
         if self.tail is not None:
@@ -25,23 +28,75 @@ class doublyLinkedList:
         else:
             self.head = newNode
         self.tail = newNode
+        self.size += 1
 
+    def pop_front(self):
+        if self.size < 1:
+            print("Error! List is empty. Can't remove any element.")
+            return
+        if self.size == 1:
+            self.head = None
+            self.tail = None
+            self.size -= 1
+            return
+        firstNode = self.head
+        firstNode.next.prev = None
+        self.head = firstNode.next
+        firstNode.next = None
+        firstNode = None
+        self.size -= 1
+    
+    def pop(self):
+        if self.size < 1:
+            print("Error! List is empty. Can't remove any element.")
+            return
+        if self.size == 1:
+            self.head = None
+            self.tail = None
+            size -= 1
+            return
+        lastNode = self.tail
+        lastNode.prev.next = None
+        self.tail = lastNode.prev
+        lastNode.prev = None
+        lastNode = None
+        self.size -= 1
+    
+    def find(self, x:int) -> int:
+        curNode = self.head 
+        i = 0
+        while curNode is not None:
+            if curNode.data == x:
+                return i
+            curNode = curNode.next
+            i += 1
+        return -1
+
+    def reverse(self):
+        if self.size <= 1:
+            return
+        curNode = self.head
+        while curNode is not None:
+            curNode.prev, curNode.next = curNode.next, curNode.prev
+            curNode = curNode.prev
+        self.head, self.tail = self.tail, self.head
+           
     def display(self):
         if self.head is None:
             print("Error! List is empty. Can't print empty list.")
             return
         curr = self.head
         while curr is not None:
-            print(f"{curr.data}", end = "" if curr.next is None else " -> ")
+            print(f"{curr.data}", end = "" if curr.next is None else " <-> ")
             curr = curr.next
         print()
     
 if __name__ == "__main__":
-    lst = doublyLinkedList()
+    dll = doublyLinkedList()
     for x in range(5):
-        lst.push_back(x+1)
-    lst.display()
-
+        dll.push(x+1)
+    dll.display()
+    print(dll.find(1))
         
 
 
