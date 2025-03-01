9a3e9149aba019ce7a95ab08e82b339a0088f00a
Gitter150 <chinmaypanghri@gmail.com>
2025-03-01 16:58:15 +0530

    Created an autocorrect software with python

---
+++ autocorrect.py
@@ -0,0 +1,24 @@
+# AUTOCORRECT SOFTWARE WITH PYTHON #
+import requests
+import json
+import os
+os.system('cls')
+prompt = input("Enter the sentence to be corrected!\n")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
+headers = {"Content-Type": "application/json"}
+data =  {
+            "contents": 
+                    [{
+                        "parts":[{"text": f'Correct the typo: "{prompt}", return a single straightforward autocorrected string without any filler statements'}]
+                    }]
+        }
+response = requests.post(url, headers=headers, json = data).json()
+response_text = response["candidates"][0]["content"]["parts"][0]["text"]
+print(f"Corrected sentence is\n{response_text.replace("\n","")}")
+
+
+
+
+
