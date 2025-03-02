3f503bc44a9fb1e68c5c58d53281d7b4b35847e2
Gitter150 <chinmaypanghri@gmail.com>
2025-03-02 09:58:10 +0530

    Added a feature to see the number of tokens used

--- autocorrect.py
+++ autocorrect.py
@@ -15,8 +15,14 @@ data =  {
                     }]
         }
 response = requests.post(url, headers=headers, json = data).json()
+#print(json.dumps(response, indent=4))
 response_text = response["candidates"][0]["content"]["parts"][0]["text"]
+promptTokenCount = response["usageMetadata"]["promptTokenCount"]
+candidatesTokenCount = response["usageMetadata"]["candidatesTokenCount"]
+totalTokenCount = response["usageMetadata"]["totalTokenCount"]
 print(f"Corrected sentence is\n{response_text.replace("\n","")}")
+print(f"Candidate tokens used: {candidatesTokenCount}\nPrompt tokens used: {promptTokenCount}")
+print(f"Total tokens used: {totalTokenCount}")
 
 
 
